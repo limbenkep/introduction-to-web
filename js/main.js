6052c7e15f7c2318ee1486bc2253282ab35ee6f6
Lima Honorine <holi1900@student.miun.se>
2020-05-07 18:38:40 +0200

    creates index.html - writes codes for text area in main creates style.css -styles textarea box creates main.js - creat text obect,functions displayText, main

---
+++ js/main.js
@@ -0,0 +1,87 @@
+
+var textnr1 = {
+    title: 'Förändringens Tid',
+    author: 'Erik Ström',
+    language: 'swedish',
+    text: 'Vinden viner över sällsamma ruiner, över berg och slätter, dagar som nätter. Ger världen form inför den kommande storm, likt gudars sång, skall bli dess undergång. Svart som natten, blank likt vatten, i skyn du häver då Allfader kräver. Åter resas skall nu han, som i misteln döden fann. Sonas med sin ene broder, den blinde född av samma moder. Satt att råda är de båda, bröders hand över evigt land.',
+    textTitle: function () {
+        return this.title;
+    },
+    authorName: function () {
+        return this.author;
+    },
+    textLanguage: function () {
+        return this.authorName()
+    },
+    textContent: function () {
+        return this.text;
+    }
+}
+
+var textnr2 = {
+    title: 'Moln',
+    author: 'Karin Boye',
+    language: 'swedish',
+    text: 'Se de mäktiga moln, vilkas fjärran höga toppar stolta, skimrande resa sig, vita som vit snö! Lugna glida de fram för att slutligen lugnt dö sakta lösande sig i en skur av svala droppar. Majestätiska moln - genom livet, genom döden gå de leende fram i en strålande sols sken utan skymmande oro i eter så klart ren, gå med storstilat, stilla förakt för sina öden.',
+    textTitle: function () {
+        return this.title;
+    },
+    authorName: function () {
+        return this.author;
+    },
+    textLanguage: function () {
+        return this.authorName()
+    },
+    textContent: function () {
+        return this.text;
+    }
+}
+
+var textnr3 = {
+    title: 'Jag har en dröm',
+    author: 'Martin Luther King Jr.',
+    language: 'swedish',
+    text: 'Så säger jag er, mina vänner, att jag trots dagens och morgondagens svårigheter har en dröm. Det är en dröm med djupa rötter i den amerikanska drömmen om att denna nation en dag kommer att resa sig och leva ut den övertygelsens innersta mening, som vi håller för självklar: Att alla människor är skapade med samma värde.',
+    textTitle: function () {
+        return this.title;
+    },
+    authorName: function () {
+        return this.author;
+    },
+    textLanguage: function () {
+        return this.authorName()
+    },
+    textContent: function () {
+        return this.text;
+    }
+}
+
+
+function displaytext() {
+
+    var selected = document.getElementById("text_choices").value;
+
+    switch (selected) {
+        case "1":
+            document.getElementById("text_title").innerText = textnr1.textTitle();
+            document.getElementById("text_author").innerText = textnr1.authorName();
+            document.getElementById("text_content").innerText = textnr1.textContent();
+            break;
+        case "2":
+            document.getElementById("text_title").innerText = textnr2.textTitle();
+            document.getElementById("text_author").innerText = textnr2.authorName();
+            document.getElementById("text_content").innerText = textnr2.textContent();
+            break;
+        case "3":
+            document.getElementById("text_title").innerText = textnr3.textTitle();
+            document.getElementById("text_author").innerText = textnr3.authorName();
+            document.getElementById("text_content").innerText = textnr3.textContent();
+            break;
+    }
+}
+function main() {
+    document.getElementById("myTextarea").readOnly = true;
+}
+
+window.addEventListener("change", displaytext);
+window.addEventListener("load", main, false);
